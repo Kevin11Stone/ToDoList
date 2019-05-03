6359212f2e817964a8b91ff091d25cd4928d15ea
Kevin Stone <ruca6616@gmail.com>
2019-05-03 08:40:51 -0700

    Add ToDo item in progress

--- typescript/main.ts
+++ typescript/main.ts
@@ -24,3 +24,55 @@ if (testItem.isComplete) {
     
 }
 */
+
+// when add item is clicked, get data off the page and 
+    // wrap in ToDo object
+    // notify user and clear form, save ToDo object
+
+window.onload = function() {
+    let addBtn = <HTMLElement>document.querySelector("#create-item > button");
+    addBtn.onclick = processNewItem;
+}
+
+
+function processNewItem() {
+    let item:ToDoItem = getItemFromForm();
+    saveItem(item);
+    notifyUser();
+    clearForm();
+}
+
+
+
+function saveItem(item:ToDoItem):void {
+    // ensure user can use local storage
+    if(typeof(Storage) !== "undefined") {
+        localStorage.setItem("todo", item.title);
+    }
+}
+
+
+
+
+/**
+ * Get all user input from Form and wrap it in a ToDo item
+ */
+function getItemFromForm():ToDoItem {
+    let item =new ToDoItem();
+
+    item.title = (<HTMLInputElement>document.getElementById("title")).value;
+    item.description = (<HTMLTextAreaElement>document.getElementById("description")).value;
+
+    let itemStartDate:string = (<HTMLInputElement>document.getElementById("start-date")).value;
+    item.startDate = new Date ( itemStartDate );
+
+    let itemEndDate:string = (<HTMLInputElement>document.getElementById("end-date")).value;
+    item.endDate = new Date ( itemEndDate );
+
+    item.isComplete = (<HTMLInputElement>document.getElementById("is-complete")).checked;
+
+    let urgencyElement = <HTMLSelectElement>document.getElementById("urgency");
+    item.urgency = urgencyElement.options[urgencyElement.selectedIndex].text;
+
+    return item;
+}
